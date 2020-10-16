46ce0cc162b4ebb03ef9d285e70488b1439f8695
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 11:10:05 -0500

    Create reto3.py

---
+++ reto3.py
@@ -0,0 +1,17 @@
+''''
+-----------------------------
+ RETO N°3
+ Operadores y variables
+-----------------------------
+ A continuación una historia:
+ "Érase una vez en la Tierra de las Manzanas, Juan tenía tres manzanas, María tenía cinco manzanas, y Adán tenía seis manzanas. Todos eran muy felices y vivieron por muchísimo tiempo. Fin."
+
+Tu tarea es:
+ 1. Crear las variables: juan, maria, y adan.
+ 2. Asignar valores con el número de manzanas que tienen.
+ 3. Imprimir las variables en una línea, y separarlas con comas.
+ 4. Crear la variable totalManzanas, e igualarla a la suma de las tres variables anteriores.
+ 5. Imprime el valor almacenado en totalManzanas .
+ 6. ¡Experimenta con tu código! Crea nuevas variables, asígnales valores y realiza operaciones aritméticas con ellas (+, -, *, /, //, etc.). Intenta juntar una cadena con un entero en la misma línea ("Numero Total de Manzanas: " y totalManzanas).
+-----------------------------
+'''
