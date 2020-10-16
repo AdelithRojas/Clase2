9d73347a627cbb37cd8701bd3a4d0ff9532a3a70
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 09:48:04 -0500

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+''''
+-----------------------------
+ EJERCICIO N°1
+ Operadores aritméticos
+-----------------------------
+ EXPONENCIACIÓN **
+-----------------------------
+'''
+print(4 ** 2)
+print(4 ** 2.)
+print(4. ** 2)
+print(4. ** 2.)
+
+'''
+-----------------------------
+ 1. Cambiemos el operador por MULTIPLICACIÓN * y DIVISIÓN /
+ 2. Cambiemos los números por divisiones que resulten en decimales
+ 3. Finalmente, probemos la DIVISIÓN ENTERA //
+-----------------------------
+'''
