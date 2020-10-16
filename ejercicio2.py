93db98660ad3f98fa97e36f051f9ba2240426093
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 09:49:17 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+''''
+-----------------------------
+ EJERCICIO N°2
+ Resumen de operadores aritméticos
+-----------------------------
+ Predice cuáles serán los resultados para los siguientes fragmentos
+-----------------------------
+'''
+print((2**4), (2*4.), (2*4))
+#print((-2/4), (2/4), (2//4), (-2//4))
+#print((2%-4), (2%4), (2**3**2))
