a1d49732747bf3a46607427bd0830cee3f82262f
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-16 11:09:15 -0500

    Create reto2.py

---
+++ reto2.py
@@ -0,0 +1,15 @@
+''''
+-----------------------------
+ RETO N°2
+ Operadores y expresiones
+-----------------------------
+ Este código lee un valor flotante, lo coloca en una variable "x" e imprime el valor de la variable "y". Tu tarea es completar el código para evaluar la siguiente expresión:
+ 3x^3 - 2x^2 + 3x - 1
+ y asignar el resultado a "y".
+-----------------------------
+'''
+
+# codifica aquí tus datos de prueba.
+x = float(x)
+# escribe tu código aquí.
+print("y =", y)
